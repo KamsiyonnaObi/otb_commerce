9b0c2418620d15b2ed4a8cd7ed623306b8c730e5
kamsi <obikamsi@gmail.com>
2023-03-13 10:28:30 -0500

    ecommerce project beginnings

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-const index = () => {
+import { client } from '../lib/client'
+import { Product, FooterBanner, HeroBanner } from '../components'
+
+const Home = ({ products, bannerData}) => {
+
   return (
-    <div>index</div>
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[1]} />
+
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>On The Block Podcast Official Merchandise!</p>
+      </div>
+
+      <div className="products-container">
+        {products?.map((product) => <Product key= {product._id} product={product} />)}
+      </div>
+
+      <FooterBanner  footerBanner={bannerData && bannerData[0]}/>
+    </>
   )
 }
 
-export default index
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
+export default Home
